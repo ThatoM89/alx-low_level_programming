c7643ff36026701096bfa0b402f1e8cce50369e1
stevens-benton masenya <s.t.masenya@gmail.com>
2023-02-22 10:55:48 -0800

    nested loop task.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,25 +1,19 @@
 #include "main.h"
+/**
+ * _isalpha - checks for char alphabet
+ * @c : the char to be checked
+ *
+ * Return :1 if  char is letter lower oe upper, 0 otherwise
+ */
 
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z'|| (c >= 'A' && <= 'z'))
+		return (1);
+	else
+	
+	return (0);
+	
 
-	/**
-	 * main - check the code.
-	 *
-	 * Return: Always 0.
-	 */
-	int main(void)
-	{
-	    int r;
-
-
-	    r = _isalpha('H');
-	    _putchar(r + '0');
-	    r = _isalpha('o');
-	    _putchar(r + '0');
-	    r = _isalpha(108);
-	    _putchar(r + '0');
-	    r = _isalpha(';');
-	    _putchar(r + '0');
-	    _putchar('\n');
-	    return (0);
-	}
+}
 
